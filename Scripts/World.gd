258af0e85ddedda4fb50fdac85e59bb44c2060de
arturgaluszka <manstrike@gmail.com>
2018-03-22 23:16:14 +0100

    change camera to move smoothly

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -8,13 +8,14 @@ func _process(delta):
 	
 func _update_GUI():
 	var player = get_node("KinematicBody2D")
-	var label = get_node("KinematicBody2D/Camera2D/Label")
+	#var label = get_node("KinematicBody2D/Camera2D/Label")
+	var label = get_node("CanvasLayer/Label")
 	
 	label.text = _get_power_text(player)
 
 func _get_power_text(player):
 	var POWER = player.POWER
 	if POWER <= 9000:
-		return "POWER: " + str(POWER) + "#" + str(player.VELOCITY)
+		return "POWER: " + str(POWER)
 	else:
 		return "POWER: 9000+"
